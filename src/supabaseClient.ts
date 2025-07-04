b82da6039f5c3d043d3ea11cfc00a82f38d2541f
Ana Franco <alfranco@uwaterloo.ca>
2025-07-03 23:00:41 -0400

    Env file set up, logo added

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import 'dotenv/config';
 
-const supabaseUrl = 'https://knzzlcxeyzqvgrwhnjyw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuenpsY3hleXpxdmdyd2huanl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0MDE3NDQsImV4cCI6MjA2Njk3Nzc0NH0.RX3cYTeDSwe6dvw5qIHi3W-yBBsntopvqDfinCNvffs';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
+export const supabase = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
+  );
