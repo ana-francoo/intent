2691d73a78248873c65e058ceb7420b1b8d17019
sunniekapar <kaparsunnie@gmail.com>
2025-08-02 15:18:25 -0700

    chore: remove unused styles in flame

--- src/components/home/Flame.tsx
+++ src/components/home/Flame.tsx
@@ -1,8 +1,10 @@
-export default function Flame() {
+import { cn } from "@/lib/utils";
+
+export default function Flame({ className }: { className?: string }) {
   return (
-    <div className="relative left-1/2 -translate-x-1/2 scale-y-[0.6] scale-x-[0.7] bottom-[55%] [&_*:before]:absolute [&_*:before]:content-[''] [&_*:after]:absolute [&_*:after]:content-['']">
+    <div className={cn("relative left-1/2 -translate-x-1/2", className)}>
       {/* Glow */}
-      <div className="absolute w-25 h-44 left-1/2 -top-25 -translate-x-1/2 rounded-full bg-orange-600 blur-3xl opacity-0 animate-[var(--animate-glow-fade-delayed),var(--animate-blink)]"/>
+      <div className="absolute size-25 left-1/2 -top-25 -translate-x-1/2 rounded-full bg-orange-600 blur-3xl opacity-0 animate-[var(--animate-glow-fade-delayed),var(--animate-blink)]"/>
       {/* Blue flame */}
       <div className="absolute w-6.5 h-11 rounded-[100%_100%_100%_100%] left-1/2 bottom-0 -translate-x-1/2 bg-blue-500/70 shadow-[0_-40px_30px_0_#dc8a0c,0_40px_50px_0_#dc8a0c,inset_3px_0_2px_0_rgba(0,133,255,0.6),inset_-3px_0_2px_0_rgba(0,133,255,0.6)] opacity-0 animate-[var(--animate-glow-fade)] before:w-[70%] before:h-[60%] before:left-1/2 before:-translate-x-1/2 before:bottom-0 before:rounded-full before:bg-black/35"/>
       {/* Orange flame */}
