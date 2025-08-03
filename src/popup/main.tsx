c3ee523be2bbdfc0a3a28e061eb5d9a1347d1baf
sunniekapar <kaparsunnie@gmail.com>
2025-08-03 02:07:09 -0700

    chore: remove unused import

--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import '@/index.css'
-import './App.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
