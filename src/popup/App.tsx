1aea12e1c2525aae8168260c3f2c31966d396db9
Ana Franco <alfranco@uwaterloo.ca>
2025-08-04 00:33:10 -0400

    Personal dashboard popover added, flow of home -> carousel fix

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -3,6 +3,8 @@ import Home from '@/components/home/Home'
 import Auth from '@/components/auth/Auth'
 import WebsiteBlocking from '@/components/website-blocking/WebsiteBlocking'
 import PopoverDashboard from '@/components/main-dashboard/PopoverDashboard'
+import PersonalDashboard from '@/components/PersonalDashboard'
+import Welcome from '@/components/Welcome'
 import Smoke from '@/components/smoke-test/Smoke'
 import IntentionOverlay from '@/components/overlay'
 import CarouselApp from '@/components/carousel/App'
@@ -10,7 +12,8 @@ import CarouselApp from '@/components/carousel/App'
 export default function App() {
   return (
     <Routes>
-        <Route path="/" element={<PopoverDashboard />} />
+        <Route path="/" element={<PersonalDashboard />} />
+        <Route path="/welcome" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/onboarding" element={<CarouselApp />} />
         <Route path="/auth" element={<Auth onAuthSuccess={() => {}} />} />
